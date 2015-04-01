54ad3f7512098f9e650e01706d03e960f0848296
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-04-01 15:37:01 -0500

    switch to eslint

--- test/helpers/phantomjs-shims.js
+++ test/helpers/phantomjs-shims.js
@@ -16,7 +16,6 @@ require('babel/browser-polyfill');
 			var args = slice.call(arguments, 1);
 
 			function bound() {
-				/*jshint -W040 */
 				var invokedAsConstructor = func.prototype && (this instanceof func);
 				return func.apply(
 					// Ignore the context parameter when invoking the bound function
@@ -26,7 +25,6 @@ require('babel/browser-polyfill');
 					!invokedAsConstructor && context || this,
 					args.concat(slice.call(arguments))
 				);
-				/*jshint +W040 */
 			}
 
 			// The bound function must share the .prototype of the unbound
@@ -37,4 +35,4 @@ require('babel/browser-polyfill');
 			return bound;
 		};
 	}
-})();
+}());
