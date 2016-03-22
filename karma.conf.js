ac0825b02abe05c49802f6f62eb351516ecc02ad
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-03-22 14:49:49 -0500

    Use shared unittest configs

--- karma.conf.js
+++ karma.conf.js
@@ -1,137 +1,15 @@
-/*eslint no-var: 0, strict: 0*/
-'use strict';
-var webpack = require('webpack');
+const baseConfig = require('nti-unittesting-clientside');
 
 module.exports = function (config) {
-	config.set({
-		basePath: '',
-		frameworks: ['jasmine'],
+	baseConfig.webpack.resolve.root = void 0;
 
+	config.set(Object.assign(baseConfig, {
 		files: [
-			'test/**/*'
+			'test/**/*.js'
 		],
 
 		preprocessors: {
-			'test/**/*': ['webpack', 'sourcemap']
-		},
-
-		exclude: [],
-
-		port: 8090,
-		logLevel: config.LOG_WARN,
-		colors: true,
-		autoWatch: false,
-		// Start these browsers, currently available:
-		// - Chrome
-		// - ChromeCanary
-		// - Firefox
-		// - Opera
-		// - Safari (only Mac)
-		// - PhantomJS
-		// - IE (only Windows)
-		browsers: ['PhantomJS'],
-
-
-		coverageReporter: {
-			dir: 'reports/coverage/',
-			reporters: [
-				{ type: 'html', subdir: 'html' },
-				{ type: 'lcov', subdir: 'lcov' },
-				{ type: 'cobertura', subdir: '.', file: 'cobertura.xml' },
-				{ type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
-				{ type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
-				{ type: 'text', subdir: '.', file: 'text.txt' },
-				{ type: 'text-summary', subdir: '.', file: 'text-summary.txt' }
-			]
-		},
-
-		htmlReporter: {
-			outputDir: 'reports',
-			reportName: 'test-results'
-		},
-
-		junitReporter: {
-			outputDir: 'reports/test-results/',
-			outputFile: 'index.xml',
-			suite: 'nti-lib-whiteboardjs',
-			useBrowserName: false
-		},
-
-
-		// other possible values: 'dots', 'progress', 'junit', 'html', 'coverage'
-		reporters: ['mocha'],
-		captureTimeout: 60000,
-		singleRun: true,
-
-
-		webpackServer: {
-			noInfo: true,
-			stats: {
-				version: false,
-				hash: false,
-				timings: false,
-				assets: false,
-				chunks: false,
-				chunkModules: false,
-				chunkOrigins: false,
-				modules: false,
-				cached: false,
-				cachedAssets: false,
-				showChildren: false,
-				source: false,
-
-				colors: true,
-				reasons: true,
-				errorDetails: true
-			}
-		},
-
-		webpack: {
-			cache: true,
-			debug: true,
-			devtool: 'inline-source-map',
-
-			node: {
-				net: 'empty',
-				tls: 'empty'
-			},
-
-			resolve: {
-				root: __dirname,
-				extensions: ['', '.js']
-			},
-
-			plugins: [
-				new webpack.DefinePlugin({
-					SERVER: false
-				})
-			],
-
-			module: {
-				preLoaders: [
-					{
-						test: /\.js(x)?$/,
-						loader: 'isparta-instrumenter',
-						exclude: [
-							/node_modules/,
-							/shims.js$/,
-							/spec.js$/,
-							/__test__/,
-							/test\/main/
-						]
-					}
-				],
-				loaders: [
-					{ test: /\.json$/, loader: 'json' },
-					{
-						test: /\.js(x)?$/,
-						loader: 'babel',
-						exclude:[
-							/node_modules/
-						]
-					}
-				]
-			}
+			'test/**/*.js': ['webpack', 'sourcemap']
 		}
-	});
+	}));
 };
