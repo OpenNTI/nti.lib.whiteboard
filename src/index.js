1f258339e77fa074acc3200c03ee00bd91579c11
Jonathan Grimes <jsg2021@gmail.com>
2020-03-02 13:36:56 -0600

    Stop using Stage 1 proposal: `export Foo from ...`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-export Canvas from './Canvas';
-export CollisionDetection from './CollisionDetection';
-export Color from './Color';
-export Matrix from './Matrix';
-export TransformLimitReached from './TransformLimitReached';
+export { default as Canvas } from './Canvas';
+export { default as CollisionDetection } from './CollisionDetection';
+export { default as Color } from './Color';
+export { default as Matrix } from './Matrix';
+export { default as TransformLimitReached } from './TransformLimitReached';
 export * as Utils from './utils';
